1ec1389c841fab691f08801e51f6f963dcd9f433
u202010884 <90493634+u202010884@users.noreply.github.com>
2021-09-29 19:48:03 -0500

    profesorconfactoria

--- TrabajoFinal_Grupo4/src/DigitalizaciónColegio/Profesor/ProfesorFactoria.java
+++ TrabajoFinal_Grupo4/src/DigitalizaciónColegio/Profesor/ProfesorFactoria.java
@@ -1,9 +1,9 @@
 package DigitalizaciónColegio.Profesor;
 
 public class ProfesorFactoria {
-    public ProfesorFactoria crear (String tipo, String... args){
+    public Profesor crear (String tipo, String... args){
         if (tipo.equals("ProfesorTC")){
-            return new ProfesorTC(args[0],args[1],args[2],args[3],args[4],args[5],Double.parseDouble(args[6]));
+            return new ProfesorTC(args[0], args[1], args[2], args[3], args[4], args[5],Double.parseDouble(args[6]));
         }else if (tipo.equals("ProfesorTP")){
             return new ProfesorTP(args[0],args[1],args[2],args[3],args[4],args[5],Integer.parseInt(args[6]),Double.parseDouble(args[7]));
 
