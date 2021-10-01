6b92f6c4803a9ad37a4dab2d5c65fcbca1a933e1
u202010884 <90493634+u202010884@users.noreply.github.com>
2021-09-30 22:28:05 -0500

    ProfesorConException

--- TrabajoFinal_Grupo4/src/DigitalizaciónColegio/Profesor/Main.java
+++ TrabajoFinal_Grupo4/src/DigitalizaciónColegio/Profesor/Main.java
@@ -3,6 +3,7 @@ package DigitalizaciónColegio.Profesor;
 public class Main {
     public static void main(String[] args) throws SueldoMaximoException {
         Controller controller = new Controller("Marie Curie");
+
         try {
             controller.registrarProfesor("ProfesorTC", "45708997", "Juan", "Andrade", "juan.andrade@mariecourie.com", "Matematica", "1200");
             controller.registrarProfesor("ProfesorTC","56702382", "Fernando", "Ramirez", "fernando.ramirez@mariecourie.com", "Letras","1500");
@@ -10,17 +11,22 @@ public class Main {
             controller.registrarProfesor("ProfesorTC","41400572", "Luis", "Fernandez", "luis.fernandez@mariecourie.com", "Ciencias","1100");
             controller.registrarProfesor("ProfesorTC","70349323", "Christian", "Alva", "christian.alva@mariecourie.com", "Educacion Fisica","950");
             controller.registrarProfesor("ProfesorTC","39982919", "Martha", "Delgado", "martha.delgado@mariecourie.com", "Lenguaje","1180");
+            controller.registrarProfesor("ProfesorTP","50332010","Teresa","Contreras","teresa.contreras@mariecourie.com","Matematica","40","45");
+            controller.mostrarListadoProfesor();
         }catch (SueldoMaximoException e){
             System.out.println(e.getMessage());
         }
 
         try {
-            controller.registrarProfesor("ProfesorTC", "58683922", "Fabiana", "Lopez", "juan.andrade@mariecourie.com", "Matematica", "2501");
-        }catch (SueldoMaximoException e){
-            System.out.println(e.getMessage());
+            controller.registrarProfesor("ProfesorTC", "58683922", "Fabiana", "Lopez", "juan.andrade@mariecourie.com", "Matematica", "3000");
+            controller.mostrarListadoProfesor();
+        }catch (SueldoMaximoException ex){
+            System.out.println(ex.getMessage());
+            System.out.println("-------------------------");
         }
 
-        controller.mostrarListadoProfesor();
+
+
         controller.buscarXDNIProfesor();
     }
 }
